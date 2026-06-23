ad6a3302fbde884b59a95dd0a47ef8a7b1d5b3a6
Roman Vavilov <vavilov2212@gmail.com>
2026-06-23 13:52:56 -0500

    Provide PageUiPreferences and DrawerHostController at the app root

--- apps/worklog_studio/lib/feature/app/app.dart
+++ apps/worklog_studio/lib/feature/app/app.dart
@@ -18,6 +18,8 @@ import 'package:worklog_studio/feature/desktop/presentation/mini_panel.dart';
 import 'package:worklog_studio/feature/desktop/presentation/mini_tracker_cubit.dart';
 import 'package:worklog_studio/state/entity_resolver.dart';
 import 'package:worklog_studio/state/project_task_state.dart';
+import 'package:worklog_studio/state/page_ui_preferences.dart';
+import 'package:worklog_studio/state/drawer_host_controller.dart';
 import 'package:worklog_studio/feature/time_tracker/bloc/time_tracker_bloc.dart';
 import 'package:worklog_studio_style_system/ui_kit/src/drawer/drawer_service.dart';
 import 'package:worklog_studio/core/services/desktop/desktop_service_registry.dart';
@@ -66,6 +68,8 @@ class MainApp extends StatelessWidget {
         Provider<AppNavigationController>(
           create: (_) => AppNavigationController(),
         ),
+        ChangeNotifierProvider(create: (_) => PageUiPreferences()),
+        ChangeNotifierProvider(create: (_) => DrawerHostController()),
         BlocProvider<TimeTrackerBloc>(
           create: (_) {
             final clock = SystemClock();
