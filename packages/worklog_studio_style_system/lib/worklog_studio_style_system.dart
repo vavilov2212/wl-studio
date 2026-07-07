2f4760e04f405ae49f192cace6e755909a24654d
Roman Vavilov <vavilov2212@gmail.com>
2026-07-07 15:25:38 -0500

    refactor(item-27): move badge color palette to style system package
    Created badge_palette.dart in the style system with kBadgePalette const
    list of 14 bg/text Color pairs. Exported via the package barrel.
    Updated BadgeUtils to import kBadgePalette from the style system instead
    of defining it inline, removing all hardcoded Color literals from the app.
    
    264/264 tests pass.

--- packages/worklog_studio_style_system/lib/worklog_studio_style_system.dart
+++ packages/worklog_studio_style_system/lib/worklog_studio_style_system.dart
@@ -1,6 +1,7 @@
 library worklog_studio_style_system;
 
 export 'resources/assets.gen.dart' show WorklogStudioAssets;
+export 'theme/colors_palette/badge_palette.dart' show kBadgePalette;
 export 'theme/app_theme.dart' show AppTheme;
 export 'theme/control_size.dart' show ControlSize, ControlSizeTokens;
 export 'theme/radiuses.dart' show CircularBorderRadius;
